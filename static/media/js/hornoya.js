19f8a2cba7a3192b4a49b083127df76338c82c7c
Henrik Lied <Henrik.Lied@nrk.no>
2016-05-31 11:31:48 +0200

    Ut med personsuper automatisk

--- static/media/js/hornoya.js
+++ static/media/js/hornoya.js
@@ -1,5 +1,4 @@
 $(document).on('infosuper', function(e, s) {
-});
 $(document).on('personsuper', function(e, s) {
 	$("#super_"+s.id).find('.title span').delay(1000).animate({top: "0", opacity: 1}, 500);
 	$("#super_"+s.id).find('.name span').delay(1000).animate({bottom: "0", opacity: 1}, 500);
