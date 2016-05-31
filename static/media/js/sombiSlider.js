dfe6f10d422bfb10726c752259a95259207d76a4
Henrik Lied <Henrik.Lied@nrk.no>
2016-05-31 09:39:20 +0200

    z-index-endringer

--- static/media/js/sombiSlider.js
+++ static/media/js/sombiSlider.js
@@ -4,9 +4,11 @@ function loadSlider() {
 	$('.sombiSlider figure').addClass("next").last().removeClass("next").addClass("current");
 
 	sombiSliderInterval = setInterval(function() {
+		$('.sombiSlider figure').css('z-index', 300);
 		var nxt = $('.sombiSlider figure.current')
 			.removeClass("current")
 			.addClass("prev")
+			.css('z-index', 9999)
 			.prev();
 		if (nxt.length == 0) {
 			return;
