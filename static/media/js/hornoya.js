1419d8cf1ba6f0c3d5b482253c54d27566713bc3
Henrik Lied <Henrik.Lied@nrk.no>
2016-05-31 11:46:30 +0200

    Endringer

--- static/media/js/hornoya.js
+++ static/media/js/hornoya.js
@@ -1,5 +1,6 @@
-$(document).on('infosuper', function(e, s) {
+$(document).on('infosuper', function(e, s) {});
 $(document).on('personsuper', function(e, s) {
+	console.log("Fyrer");
 	$("#super_"+s.id).find('.title span').delay(1000).animate({top: "0", opacity: 1}, 500);
 	$("#super_"+s.id).find('.name span').delay(1000).animate({bottom: "0", opacity: 1}, 500);
 	$('<video src="media/gfx/hornoya/uin.webm" autoplay></video>').appendTo("#super_"+s.id);
